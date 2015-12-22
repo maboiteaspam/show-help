a52d5f341fe86fb6f37ae72c4afe6403e425798a
Clement Auger <clement.ager@d1m.cn>
2015-12-22 16:50:25 +0800

    README update, improved examples

--- hello-minimist.js
+++ hello-minimist.js
@@ -13,10 +13,18 @@ function usage () {/*
  ...
  */}
 
-var pkg  = require('./package.json')
-var argv = require('minimist')(process.argv.slice(2));
-require('./index.js').tpl('%name %version\n\t%description\n\n%usage')(usage, argv.h||argv.help, pkg)
-//require('./index.js')(usage, argv.h||argv.help, pkg)
+var pkg       = require('./package.json')                           // always useful.
+var argv      = require('minimist')(process.argv.slice(2));         // parse args with minimist,
+var showHelp  = require('./index.js')                               // load showHelp,
+  .tpl('%name %version\n\t%description\n\n%usage')                  // set a different template.
+  (usage, argv.h||argv.help, pkg)                                   // Print help and quit,
+                                                                    // if -h|--help.
+
+//require('./index.js')(usage, argv.h||argv.help, pkg)              // one-liner
+
+!argv['_']                                                          // if arguments are incorrect,
+&& showHelp.print(usage, pkg)                                       // show help,
+&& showHelp.die();                                                  // exit.
 
 console.log('program execution')
 
